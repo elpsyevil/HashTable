edd25a11a24f87619e7c9abc35529d7a1aedd010
Azrood <53349565+Azrood@users.noreply.github.com>
2020-11-26 20:52:50 +0100

    add header file

--- functions.h
+++ functions.h
@@ -1,1 +1,13 @@
+#define SIZE 20
 
+typedef struct DataItem {
+   int age;
+   char* name; //key
+}DataItem;
+
+DataItem* hashArray[SIZE]; 
+int hashFct(char);
+DataItem* search(char *);
+void insert(char *,int);
+void display();
+void delete(DataItem*);
